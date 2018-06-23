665ca3a3327039a31cd92cb70e15d4ede67a06b3
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 00:29:05 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,13 +2,15 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  while i < collection.size
-    if yield(collection[i] ) != true
-      false
-      break
-    end
-    i += 1
-    
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
