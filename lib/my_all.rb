9b226d7c4826e23325de81da3d062a0973da3b30
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 00:29:39 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,8 +5,7 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length
     block_return_values << yield(collection[i])
-    i = i + 1
+    i += 1
   end
-
   !block_return_values.include?(false)
 end
